3ec729c81fe1fbc8d5ed9663e0c7ccc5971de1c4
rickale <ti.ricardo.sousa@gmail.com>
2019-03-11 14:46:30 -0300

    Criado campos da tela de cadastro e menus na actionbar ok e cancelar

--- app/src/main/java/br/com/ricardo/carteiraclientes/CadastroCliente.java
+++ app/src/main/java/br/com/ricardo/carteiraclientes/CadastroCliente.java
@@ -1,15 +1,19 @@
 package br.com.ricardo.carteiraclientes;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CadastroCliente extends AppCompatActivity {
 
-    private FloatingActionButton fab;
+    private EditText editNome;
+    private EditText editEndereco;
+    private EditText editTelefone;
+    private EditText editEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +22,37 @@ public class CadastroCliente extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        editNome = (EditText) findViewById(R.id.editNome);
+        editEndereco = (EditText) findViewById(R.id.editEndereco);
+        editTelefone = (EditText) findViewById(R.id.editTelefone);
+        editEmail = (EditText) findViewById(R.id.editEmail);
 
-            }
-        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_cadastro_clientes, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_ok) {
+            Toast.makeText(this, "OK", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if(id == R.id.action_cancelar) {
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
